f393c2d72f990c4ccb9999fc11af90353e6aeed7
Фадеев Тимур Сергеевич <110234293+fynkyfaya@users.noreply.github.com>
2022-12-25 22:35:32 +0200

    Add files via upload

---
+++ Program (19).cs
@@ -0,0 +1,30 @@
+﻿Rectangle rectangle = new Rectangle(
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine()),
+    int.Parse(Console.ReadLine())
+);
+
+Console.WriteLine(rectangle.isSquare());
+
+
+class Rectangle
+{
+    public int x1;
+    public int x2;
+    public int y1;
+    public int y2;
+
+    public Rectangle(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+    }
+
+    public bool isSquare()
+    {
+        return x2 - x1 == y2 - y1;
+    }
+}
